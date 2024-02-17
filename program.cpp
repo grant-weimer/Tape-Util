cb0024748056d9898939c8624f1bb44c3e45f462
gweim <grantmweimer@gmail.com>
2024-02-16 18:59:12 -0700

    V1

---
+++ program.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Song {
+    public:
+    string name;
+    int min, sec;
+    
+    Song(string n = "", string length = "0:00"){
+        name = n;
+        sec = stoi(length.substr(length.size()-2));
+        min = stoi(length.substr(0, length.size()- 3));
+    }
+    Song operator+(Song const& s2){
+            Song result;
+            if( (this->sec + s2.sec) >= 60){
+                result.sec = this->sec + s2.sec - 60;
+                result.min = this->min + s2.min + 1;
+            }
+            else{
+                result.sec = this->sec + s2.sec;
+                result.min = this->min + s2.min;
+            }
+            return result;
+        }
+};
+
+int main(){
+    cout << "Welcome to Grant's Cassette maker!\n Please enter track information and press q when done";
+    string name;
+    string l;
+    vector<Song> tracks;
+    while(true){
+        cout << "Track name (q to finish): ";
+        cin.ignore();
+        getline(cin, name);
+        if(name == "q") break;
+        cout << "Track length: ";
+        cin >> l;
+        tracks.push_back(Song(name, l));
+    }
+    Song counterSong;
+    cout << "\n-----SIDE A-----";
+    for (Song s : tracks) {
+        if((counterSong + s).min > 30){
+            cout << "\nSIDE A LENGTH: " << counterSong.min << ":" << counterSong.sec << endl;
+            //cout << "\nEMPTY SPACE: " << 30-counterSong.min << ":" << 
+            cout << "\n-----SIDE B-----\n";
+            counterSong = Song();
+        }
+        counterSong = counterSong + s;
+        //cout << counterSong.min << " " << counterSong.sec << "\n";
+        cout << "Name: " << s.name << " length: " << s.min << ":" << s.sec << endl;
+    }
+    return 1;
+    //TODO fix the program
+    //Add file reading
+    //Add error handling for input
+    //Add Side b length info
+    //add message for if tape is too long
+}
